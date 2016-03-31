1a3fb567a2c0697acca820cdc4b8d91b65940d47
Cam <camlatimer@gmail.com>
2016-03-31 09:42:12 -0400

    started controller files for index controller and show controller

--- angular_app/js/app.js
+++ angular_app/js/app.js
@@ -10,6 +10,18 @@
   .config(['$stateProvider', RouteBuilder]);
 
   function RouteBuilder($stateProvider){
-
+    $stateProvider
+    .state("songsIndex", {
+      url: "/songs",
+      templateUrl:"js/songs/index.html",
+      controller: "songsIndexController",
+      controllerAs: "songsIndexVM"
+    })
+    .state("songsShow", {
+      url: "/songs/:id",
+      templateUrl: "js/songs/show.html",
+      controller: "songsShowController",
+      controllerAs: "songsVM"
+    })
   }
 }());
---
+++ angular_app/js/songs/songsIndex.controller.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function(){
+
+  angular
+  .module('songs')
+  .controller('songsIndexController', [songsIndexControllerFunction]);
+
+  function songsIndexControllerFunction(){
+    
+  }
+
+}());
